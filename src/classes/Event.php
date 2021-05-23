33306dc0a20e30d99f62f47805c00dd239e9c2f7
StefanoBavota <stefano.bavota@hotmail.com>
2021-05-23 16:57:57 +0200

    add create event function

--- src/classes/Event.php
+++ src/classes/Event.php
@@ -1,9 +1,14 @@
 <?php
 
 namespace App\Classes;
+require_once __DIR__ . '/DBManager.php';
 
-class Event {
-    public function createEvent() {
-
+class Event extends DBManager{
+    public function createEvent($image, $name, $description, $data, $user_id) {
+        $resultSet = $this->db->execute("INSERT INTO eventi (image, name, description, data, user_id) VALUES ('$image', '$name', '$description', '$data', $user_id)");
+        if (!$resultSet) {
+            return array('error' => 'Hai già inserito l\'evento');
+        }
+        return array('error' => '');
     }
 }
