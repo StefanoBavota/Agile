07d6ea5639d65c3252747df1c70e8bcb56a570d8
StefanoBavota <stefano.bavota@hotmail.com>
2021-05-24 11:18:01 +0200

    test

--- tests/createEventTest.php
+++ tests/createEventTest.php
@@ -2,7 +2,9 @@
 
 use PHPUnit\Framework\TestCase;
 use App\Classes\Event;
+use App\Classes\UserManager;
 
+require_once __DIR__ . '/../src/classes/UserManager.php';
 require_once __DIR__ . '/../src/classes/Event.php';
 require_once __DIR__ . '/../src/inc/init.php';
 
@@ -20,17 +22,22 @@ class createEventTest extends TestCase
         $connection->query("use information_schema");
         $tables = $connection->query("SELECT TABLE_NAME FROM TABLES WHERE TABLE_SCHEMA = '" . $_ENV['DB_DATABASE'] . "';");
         $connection->query("use {$_ENV['DB_DATABASE']}");
-        foreach ( $tables->fetchAll(PDO::FETCH_ASSOC) as $table ) {
+        foreach ($tables->fetchAll(PDO::FETCH_ASSOC) as $table) {
             $connection->query("TRUNCATE " . $table['TABLE_NAME'] . ";");
-        } 
+        }
     }
-    
+
     public function test_createEvent()
     {
         $eventMgr = new Event();
-        $event = ['img' => 'https://images.vestiairecollective.com/cdn-cgi/image/w=1000,q=80,f=auto,/produit/11487631-1_1.jpg', 'name' => 'test', 'description' => 'test', 'data' => '2021-05-23', 'posti' => '66', , 'userId' => '3'];
-        $createdEvent = $eventMgr->createEvent($event['img'], $event['name'], $event['description'], $event['data'], $event['posti'], $event['userId']);
+        $userMgr = new UserManager;
+
+        $createdUser = $userMgr->register('Mario', 'Rossi', 'mariorossi@test.it', md5('password'));
+
+        $event = ['img' => 'image', 'name' => 'test', 'description' => 'test2', 'data' => '2021-05-23', 'posti' => '66', 'user_id' => $createdUser['id']];
+        $createdEvent = $eventMgr->createEvent($event['img'], $event['name'], $event['description'], $event['data'], $event['posti'], $event['user_id']);
         $dbEvent = $eventMgr->getEventById($createdEvent['id']);
+        
         unset($dbEvent['id']);
         $this->assertEquals($event, $dbEvent);
     }
