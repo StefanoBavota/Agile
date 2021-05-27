aa5f1d1063822d174f725186ae267ef2021c71a6
StefanoBavota <stefano.bavota@hotmail.com>
2021-05-27 18:30:50 +0200

    add fix homepage

--- src/public/index.php
+++ src/public/index.php
@@ -8,7 +8,11 @@ $page = isset($_GET["page"]) ? $_GET["page"] : "homepage";
 
 <div id="main" class="container" style="margin-top: 100px; margin-bottom: 100px;">
     <div class="row">
-        <div class="col-md-8 col-xs-12">
+        <?php if (!$loggedInUser) : ?>
+            <div class="col-md-12 col-xs-12">
+        <?php else : ?>
+            <div class="col-md-8 col-xs-12">
+        <?php endif; ?>
 
             <?php include ROOT_PATH . 'public/pages/' . $page . '.php' ?>
 
