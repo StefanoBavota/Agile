85637389ea7377171269809f471c657843b66938
StefanoBavota <stefano.bavota@hotmail.com>
2021-05-29 22:42:42 +0200

    fix carousel

--- src/public/index.php
+++ src/public/index.php
@@ -6,7 +6,9 @@ $page = isset($_GET["page"]) ? $_GET["page"] : "homepage";
 <?php include '../inc/init.php' ?>
 <?php include ROOT_PATH . 'public/template-parts/header.php' ?>
 
-<?php include ROOT_PATH . 'public/pages/carousel.php' ?>
+<?php if ($page == 'homepage') : ?>
+    <?php include ROOT_PATH . 'public/pages/carousel.php' ?>
+<?php endif; ?>
 
 <div id="main" class="container" style="margin-top: 100px; margin-bottom: 100px;">
     <div class="row">
