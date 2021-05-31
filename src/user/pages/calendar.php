6eec4615e1cdfedfbf8ae022de6de01ab1ab5dba
StefanoBavota <stefano.bavota@hotmail.com>
2021-05-31 20:03:08 +0200

    modify homepage and calendar page

--- src/user/pages/calendar.php
+++ src/user/pages/calendar.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../../classes/Event.php';
 require_once __DIR__ . '/../../classes/UserManager.php';
+
 use App\Classes\Event;
 use App\Classes\UserManager;
 
@@ -10,24 +11,23 @@ if (isset($_SESSION['user'])) {
 }
 
 $eventMgr = new Event();
-$emailUsm=new UserManager();
+$emailUsm = new UserManager();
 global $alertMsg;
 
 
 $userId = $loggedInUser->id;
 
-$email= $emailUsm->getEmailById($userId)[0]['email'];
+$email = $emailUsm->getEmailById($userId)[0]['email'];
 
 $calendarEvent = $eventMgr->getCurrentRegisterEvent($email);
 
 if (isset($_POST['filtra'])) {
 
     //$posti = htmlspecialchars(trim($_POST['posti']));
-    $mese=htmlspecialchars(trim($_POST['mese']));
-    $anno=htmlspecialchars(trim($_POST['anno']));
+    $mese = htmlspecialchars(trim($_POST['mese']));
+    $anno = htmlspecialchars(trim($_POST['anno']));
 
-    $calendarEvent = $eventMgr->filterCalendar($email,$anno,$mese);
-    
+    $calendarEvent = $eventMgr->filterCalendar($email, $anno, $mese);
 }
 
 $loader = new \Twig\Loader\FilesystemLoader('../templates');
