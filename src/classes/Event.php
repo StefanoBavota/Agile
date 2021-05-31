85ad4e13daa565665eae83d6d1721f73b9bbc7f5
StefanoBavota <stefano.bavota@hotmail.com>
2021-05-31 16:23:30 +0200

    add calendar page

--- src/classes/Event.php
+++ src/classes/Event.php
@@ -39,12 +39,12 @@ class Event extends DBManager
     }
     public function getCurrentRegisterEvent($email)
     {
-        $sql = "SELECT * FROM eventi INNER JOIN register ON register.eventi_id = eventi.id AND register.email = '$email'";
+        $sql = "SELECT * FROM eventi INNER JOIN register ON register.eventi_id = eventi.id AND register.email = '$email' ORDER by data";
         return $this->db->query($sql);
     }
     public function filterCalendar($email,$anno,$mese)
     {
-        $sql = "SELECT * FROM eventi INNER JOIN register ON register.eventi_id = eventi.id AND register.email = '$email' where eventi.data LIKE '$anno-$mese-%'";
+        $sql = "SELECT * FROM eventi INNER JOIN register ON register.eventi_id = eventi.id AND register.email = '$email' where eventi.data LIKE '$anno-$mese-%' ORDER by data";
         return $this->db->query($sql);
     }
     
